b1e9c824ef2b609200c2bb2559dc81dfa4ae8caf
Enrique Sotomayor <enrique@Enriques-Mini.hsd1.wa.comcast.net>
2020-10-14 12:36:46 -0700

    removed min password length requirement; added response to context obj for logout; implemented logout resolver

--- src/shared/PasswordInput.ts
+++ src/shared/PasswordInput.ts
@@ -1,10 +1,8 @@
 import { Field, InputType } from 'type-graphql';
-import { Min } from 'class-validator';
 
 @InputType()
 export class PasswordInput {
 
     @Field()
-    @Min(5)
     password: string;
 }
